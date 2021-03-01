a375e1af7cc61179280727ad13df0ea881586d41
Stanislav Osipkin <stan1976@mail.ru>
2021-03-01 20:18:24 +0500

    task_04_05_06_07_performed

--- Module14/07_years/main.py
+++ Module14/07_years/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+year_1 = int(input('Введите первый год: '))
+year_2 = int(input('Введите второй год: '))
+print('\nГода от', year_1, 'до', year_2, 'с тремя одинаковыми цифрами: ')
+for year in range(year_1, year_2 + 1):
+    figure = year
+    number_1 = figure % 10
+    figure //= 10
+    number_2 = figure % 10
+    figure //= 10
+    number_3 = figure % 10
+    figure //= 10
+    number_4 = figure % 10
+
+    if number_1 == number_2 and number_2 == number_3:
+        print(year)
+    elif number_1 == number_3 and number_3 == number_4:
+        print(year)
+    elif number_2 == number_3 and number_3 == number_4:
+        print(year)
+    elif number_1 == number_4 and number_2 == number_4:
+        print(year)
+
+
+
+
+
+
+
+
