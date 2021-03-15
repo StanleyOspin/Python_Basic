b0c19b173e585a010155207b8a4a6f99671db320
Stanislav Osipkin <stan1976@mail.ru>
2021-03-15 20:54:02 +0500

    task_03_04_perfomed

--- Module15/03_cells/main.py
+++ Module15/03_cells/main.py
@@ -1,15 +1,12 @@
-cells = []
 n = int(input('Кол-во клеток: '))
+cells = []
 
 for i in range(n):
     print('Эффективность', i + 1, 'клетки:', end=' ')
     efficiency = int(input())
     cells.append(efficiency)
 
-
-
-
-
-
-
-
+print('\nНеподходящие значения: ', end='')
+for index in range(len(cells)):
+    if cells[index] < index:
+        print(cells[index], end=' ')
--- Module15/04_videocards/main.py
+++ Module15/04_videocards/main.py
@@ -1,3 +1,26 @@
-# TODO здесь писать код
+def find_new_card(graphics_card):
+    new_model = graphics_card[0]
+    for model in graphics_card:
+        if new_model < model:
+            new_model = model
+            return new_model
 
 
+quantity = int(input('Кол-во видеокарт: '))
+graphics_card = []
+
+for i in range(quantity):
+    print(i + 1, 'Видеокарта:', end=' ')
+    model = int(input())
+    graphics_card.append(model)
+
+new_model = find_new_card(graphics_card)
+new_list_cards = []
+
+for model in range(len(graphics_card)):
+    if graphics_card[model] != new_model:
+        new_list_cards.append(graphics_card[model])
+
+print()
+print('\nСтарый список видеокарт: ', graphics_card)
+print('Новый список видеокарт: ', new_list_cards)
