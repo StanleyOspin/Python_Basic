7218e9a00ef5f506ccf770e1eef5ac7c141cc537
Stanislav Osipkin <stan1976@mail.ru>
2021-03-26 14:20:42 +0500

    task_05_06_perfomed

--- Module15/06_word_analysis/main.py
+++ Module15/06_word_analysis/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+word = input('Введите слово: ')
+letters = list(word)
+count = 0
+
+for i in letters:
+    repetitions = 0
+    for j in letters:
+        if i == j:
+            repetitions += 1
+    if repetitions > 1:
+        count += 1
+
+unique_letters = len(letters) - count
+print('\nКоличество уникальных букв: ', unique_letters)
