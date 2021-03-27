14c8f8559f396b75626e6575144357ab85bfe8b3
Stanislav Osipkin <stan1976@mail.ru>
2021-03-27 21:50:08 +0500

    task_07_perfomed

--- Module15/07_containers/main.py
+++ Module15/07_containers/main.py
@@ -1,1 +1,28 @@
-# TODO здесь писать код
+quantity = int(input('Кол-во контейнеров: '))
+containers_list = []
+
+for _ in range(quantity):
+    mass = int(input('Введите вес контейнера: '))
+
+    while True:
+        if mass > 0 and mass < 200:
+            break
+        else:
+            print('Ошибка. Вес контейнера должен быть больше 0, но не должен превышать 200 кг')
+            mass = int(input('Введите вес контейнера: '))
+
+    containers_list.append(mass)
+
+print(containers_list)
+print()
+mass_new = int(input('Введите вес нового контейнера: '))
+containers_list.append(mass_new)
+
+for i in range(len(containers_list)):
+    highest_value_index = i
+    for j in range(i + 1, len(containers_list)):
+        if containers_list[j] >= containers_list[highest_value_index]:
+            highest_value_index = j
+            containers_list[i], containers_list[highest_value_index] = containers_list[highest_value_index], containers_list[i]
+
+print('Номер, куда встанет новый контейнер: ' containers_list.index(mass_new) + 1)
