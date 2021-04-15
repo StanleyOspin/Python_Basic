aac6a1180d691d4b86713045cb9384745ec2f45b
Stanislav Osipkin <stan1976@mail.ru>
2021-04-15 20:06:07 +0500

    task_01_performed

--- Module16/01_scary_code/main.py
+++ Module16/01_scary_code/main.py
@@ -1,24 +1,14 @@
-a = [1, 5, 3]
-b = [1, 5, 1, 5]
-c = [1, 3, 1, 5, 3, 3]
-for i in b:
-    a.append(i)
-t = 0
-for i in a:
-    if i == 5:
-        t += 1
-print(t)
-d = []
-for i in a:
-    if i != 5:
-        d.append(i)
-for i in c:
-    d.append(i)
-t = 0
-for i in d:
-    if i == 3:
-        t += 1
-print(t)
-print(d)
+list_1 = [1, 5, 3]
+list_2 = [1, 5, 1, 5]
+list_3 = [1, 3, 1, 5, 3, 3]
 
-# TODO переписать программу
+list_1.extend(list_2)
+print('Кол-во цифр 5 при первом объединении: ', list_1.count(5))
+
+for item in list_1:
+    if item == 5:
+        list_1.remove(item)
+
+list_1.extend(list_3)
+print('Кол-во цифр 3 при втором объединении: ', list_1.count(3))
+print('Итоговый список: ', list_1)
