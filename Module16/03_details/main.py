769cad9cb29552555e5831aa1d2ea54ac143774a
Stanislav Osipkin <stan1976@mail.ru>
2021-04-15 20:25:40 +0500

    task_03_performed

--- Module16/03_details/main.py
+++ Module16/03_details/main.py
@@ -1,5 +1,18 @@
 shop = [['каретка', 1200], ['шатун', 1000], ['седло', 300],
         ['педаль', 100], ['седло', 1500], ['рама', 12000],
         ['обод', 2000], ['шатун', 200], ['седло', 2700]]
+total_cost = 0
+count = 0
+detail = input('Название детали: ')
 
-# TODO здесь писать код
+for item in shop:
+        for part in item:
+                if part == detail:
+                        total_cost += item[1]
+                        count += 1
+
+print('\nКол-во деталей - ', count)
+print('Общая стоимость - ', total_cost)
+
+
+
