79f14775f5aedea63d2d39a1ee54aeb97b91f8a1
Stanislav Osipkin <stan1976@mail.ru>
2021-04-18 22:49:32 +0500

    task_08_performed

--- Module16/08_rhyme_cnt/main.py
+++ Module16/08_rhyme_cnt/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+humans = list(range(1, int(input('Кол-во человек: ')) + 1))
+
+number = int(input('Какое число в считалке? '))
+
+print('Значит, выбывает каждый', number, 'человек')
+
+count = 0
+
+while len(humans) > 1:
+
+    print('\nТекущий круг людей:', humans)
+
+    print('Начало отсчета с номера:', humans[count])
+
+    delete = (count + number - 1) % len(humans)
+
+    if humans[delete] == humans[-1]:
+
+        count = 0
+
+    else:
+
+        count = delete
+
+    print('Выбывает человек под номером: ', humans[delete])
+
+    humans.remove(humans[delete])
+
+print('\nОстался человек под номером', humans[0])
