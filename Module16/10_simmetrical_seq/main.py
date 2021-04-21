6953590427f4d8429b83b5cee5a496742eaf05ca
Stanislav Osipkin <stan1976@mail.ru>
2021-04-21 20:33:24 +0500

    task_10_performed

--- Module16/10_simmetrical_seq/main.py
+++ Module16/10_simmetrical_seq/main.py
@@ -1,1 +1,21 @@
-# TODO здесь писать код
+number_quantity = int(input('Кол-во чисел: '))
+numbers_list = []
+
+for element in range(number_quantity):
+    number = int(input('Число: '))
+    numbers_list.append(number)
+
+print('\nПоследовательность: ', end='')
+
+for number in numbers_list:
+    print(number, end='')
+print()
+
+for i in range(len(numbers_list)):
+    if numbers_list[i:] == numbers_list[i:][::-1]:
+        print('Нужно приписать чисел: ', i)
+        break
+
+print('Сами числа: ', end='')
+for number in numbers_list[:i][::-1]:
+    print(number, end='')
