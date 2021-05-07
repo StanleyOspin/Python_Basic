97107926e858bdd887cd76379b1b5b62a2861a14
Stanislav Osipkin <stan1976@mail.ru>
2021-05-07 10:30:29 +0500

    task_02_performed

--- Module17/02_generation/main.py
+++ Module17/02_generation/main.py
@@ -1,1 +1,3 @@
-# TODO здесь писать код
+length = int(input('Введите длину списка: '))
+numbers_list = [(1 if item % 2 == 0 else item % 5) for item in range(length)]
+print('Результат:', numbers_list)
