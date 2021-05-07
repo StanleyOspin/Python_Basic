2e2613d8758f2189dd93225050a3d9e4b4b53c4a
Stanislav Osipkin <stan1976@mail.ru>
2021-05-07 10:41:23 +0500

    task_03_performed

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,11 @@
-# TODO здесь писать код
+import random
+
+team_1 = [round(random.uniform(5, 10), 2) for i_team in range(20)]
+print('Первая команда:', team_1)
+
+team_2 = [round(random.uniform(5, 10), 2) for i_team in range(20)]
+print('Вторая команда:', team_2)
+
+winners = [team_1[i_team] if team_1[i_team] > team_2[i_team] else team_2[i_team]
+           for i_team in range(20)]
+print('\nПобедители тура:', winners)
