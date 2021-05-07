872d58ec589711f30c675ada99a29390f6614d99
Stanislav Osipkin <stan1976@mail.ru>
2021-05-07 10:24:35 +0500

    task_01_performed

--- Module17/01_vowels/main.py
+++ Module17/01_vowels/main.py
@@ -1,1 +1,8 @@
-# TODO здесь писать код
+text = input('Введите текст: ')
+vowels = [letter for letter in text
+          if letter == 'а' or letter == 'о'
+          or letter == 'е' or letter == 'ё' or letter == 'и'
+          or letter == 'у' or letter == 'ы' or letter == 'э'
+          or letter == 'ю' or letter == 'я']
+print('\nСписок гласных букв: ', vowels)
+print('Длина списка: ', len(vowels))
