d0d4220a7089b06fca9154c7da75587fe6674da5
Stanislav Osipkin <stan1976@mail.ru>
2021-05-13 22:11:42 +0500

    task_10_performed

--- Module17/10_caesar_cipher/main.py
+++ Module17/10_caesar_cipher/main.py
@@ -1,1 +1,15 @@
-# TODO здесь писать код
+def caesar_cipher(string, user_shift):
+    char_list = [(alphabet[(alphabet.index(symbol) + shift) % 33] if symbol != ' ' else ' ') for symbol in string]
+    new_string = ''
+    for item in char_list:
+        new_string += item
+    return new_string
+
+
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+message = input('Введите сообщение: ')
+shift = int(input('Введите сдвиг: '))
+
+cipher_string = caesar_cipher(message, shift)
+
+print('Зашифрованное сообщение:', cipher_string)
