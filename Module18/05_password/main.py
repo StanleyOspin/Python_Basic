69c3511e5e47e0ffda4e7342c56a5fe1199b04c5
Stanislav Osipkin <stan1976@mail.ru>
2021-05-30 21:45:34 +0500

    module_18_task_05_performed

--- Module18/05_password/main.py
+++ Module18/05_password/main.py
@@ -1,1 +1,14 @@
-# TODO здесь писать код
+password = input('Придумайте пароль: ')
+upper = 0
+digit = 0
+
+for element in password:
+    if element.isupper() == True:
+        upper += 1
+    elif element.isdigit() == True:
+        digit += 1
+
+if len(password) >= 8 and upper > 0 and digit >= 3:
+    print('\nЭто надёжный пароль!')
+else:
+    print('\nПароль ненадёжный. Попробуйте ещё раз.')
