e0e19275db1c7f5286ef82f3db267c7234de1c39
Stanislav Osipkin <stan1976@mail.ru>
2021-09-15 21:21:29 +0500

    module_19_task_9_10_added

--- Module19/09_pedigree/main.py
+++ Module19/09_pedigree/main.py
@@ -1,3 +1,27 @@
+# version_1
+
+quantity = int(input('Введите количество человек: '))
+family_tree = dict()
+height = dict()
+
+for i in range(quantity - 1):
+    child, parent = input('{0} пара: '.format(i + 1)).split()
+    family_tree[child] = parent
+    height[child] = 0
+    height[parent] = 0
+
+for member in family_tree:
+    key = member
+    while key in family_tree:
+        key = family_tree[key]
+        height[member] += 1
+
+for i in sorted(height):
+    print(i, height[i])
+
+
+# version_2
+
 def height_count(member):
     if member not in family_tree:
         return 0
--- Module19/10_palindrome_again/main.py
+++ Module19/10_palindrome_again/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+string = input('Введите строку: ')
+array = set()
+
+for i in string:
+    if i in array:
+        array.remove(i)
+    else:
+        array.add(i)
+
+if len(array) > 1:
+    print('Нельзя сделать палиндромом')
+else:
+    print('Можно сделать палиндромом')
