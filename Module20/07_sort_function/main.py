12a3dc858ef0f4249a3ef82956a227b344de1a35
Alexander Olhovik <aao.t@yandex.ru>
2021-10-16 17:48:56 +0300

    урок 20, зачет (замечание в 7)

--- Module20/07_sort_function/main.py
+++ Module20/07_sort_function/main.py
@@ -2,9 +2,9 @@ def sorting(some_tuple):
     for i in some_tuple:
         if not isinstance(i, int):
             return original_tuple
-    sorted_tuple = tuple(sorted(original_tuple))
+    sorted_tuple = tuple(sorted(original_tuple))  # TODO Забыли return, сейчас возвращается None
 
 
-original_tuple = [9, 8, 7, 6, 5.5, 4, 3, 2, 1]
-# original_tuple = [9, 8, 7, 6, 5, 4, 3, 2, 1]
+# original_tuple = [9, 8, 7, 6, 5.5, 4, 3, 2, 1]
+original_tuple = [9, 8, 7, 6, 5, 4, 3, 2, 1]
 print(sorting(original_tuple))
