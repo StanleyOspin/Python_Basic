92d772bb71eca10a0418a75a73f438448894451d
Stanislav Osipkin <stan1976@mail.ru>
2021-11-19 19:39:16 +0500

    home_work_module_22

--- Module22/01_nums_sum_2/main.py
+++ Module22/01_nums_sum_2/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import os
+
+name = 'numbers.txt'
+file_path = os.path.abspath('numbers.txt')
+
+numbers_file = open(file_path, 'r')
+numbers_list = numbers_file.read().split()
+summa = sum([int(symbol) for symbol in numbers_list])
+answer_file = open('answer.txt', 'w')
+answer_file.write(str(summa))
+numbers_file.close()
+answer_file.close()
--- Module22/04_files_and_folders/main.py
+++ Module22/04_files_and_folders/main.py
@@ -1,1 +1,22 @@
-# TODO здесь писать код
+import os
+
+file_path = os.path.abspath(os.path.join('..', '02_zen_of_python', 'zen.txt'))
+
+file = open(file_path, 'r')
+string = file.read()
+
+symbol_count = [symbol.lower() for symbol in list(string) if symbol.isalpha()]
+print('Количество букв: ', len(symbol_count))
+print()
+words_count = len([word for word in string.split()])
+print('Количество слов: ', words_count)
+print()
+line_count = len([line for line in string.split('.')])
+print('Количество строк: ', line_count)
+print()
+symbol_dict = dict.fromkeys(symbol_count)
+
+for item in symbol_dict:
+    symbol_dict[item] = symbol_count.count(item)
+print('Наименьшее количество раз встречается буква', min(symbol_dict, key=symbol_dict.get))
+file.close()
