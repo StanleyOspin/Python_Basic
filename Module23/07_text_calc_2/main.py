3305e4bd5ed87ef5f409883b02c7bfb20e87def1
Stanislav Osipkin <stan1976@mail.ru>
2021-12-01 21:15:24 +0500

    home_work_module

--- Module23/07_text_calc_2/main.py
+++ Module23/07_text_calc_2/main.py
@@ -20,25 +20,23 @@ def calculation(i_line):
 
 total_sum = 0
 count = 0
-position = 0
-path_to_calc = os.path.join('Practice_23_4', 'home_work_23', 'calc.txt')
+cursor = 0
+path_to_calc = os.path.abspath('calc.txt')
 with open(path_to_calc, 'r') as calc_file:
     for i_line in calc_file:
         count += 1
-        position += len(i_line)
         string = i_line.split()
 
         try:
             total_sum += calculation(string)
         except (ZeroDivisionError, UnboundLocalError, IndexError):
             print('Обнаружена ошибка в строке №{0}:'.format(count), i_line)
-            print(position - (len(i_line) - 1))
+
             request = input('Хотите исправить? ')
             if request == 'да':
                 correct_string = input('Введите исправленную строку: ')
 
-                with open(path_to_calc, 'r+') as calc_file:
-                    calc_file.seek(position - (len(i_line) - 2))
-                    calc_file.write(str(correct_string) + '\n')
+                with open(path_to_calc, 'a') as calc_file:
+                    calc_file.write('\n' + str(correct_string))
 
     print('Сумма: ', total_sum)
