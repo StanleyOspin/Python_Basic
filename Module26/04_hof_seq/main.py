12cd915dd80e0804aa29a7b3fcf96cea405b7d2e
Stanislav Osipkin <stan1976@mail.ru>
2022-02-14 11:36:36 +0500

    home_work_26_correction_1_4_tasks

--- Module26/04_hof_seq/main.py
+++ Module26/04_hof_seq/main.py
@@ -1,9 +1,11 @@
 from typing import Iterable
 
 
-class Q_Hofstadter: # TODO Аналогично предыдущему
+class QHofstadter:
     def __init__(self, numbers: list) -> None:
         self.sequence = numbers[:]
+        if self.sequence == [1, 2]:
+            raise StopIteration
 
     def __iter__(self):
         return self
@@ -11,17 +13,13 @@ class Q_Hofstadter: # TODO Аналогично предыдущему
     def __next__(self) -> Iterable[int]:
         try:
             n = len(self.sequence)
-            if self.sequence[0] + self.sequence[1] != 3:  # TODO 1) проверку делайте в __init__
-                                                          #  2) проверка не верная, надо сраввнить self.sequerns c [1, 1]
-                q = self.sequence[n - self.sequence[n - 1]] + self.sequence[n - self.sequence[n - 2]]
-                self.sequence.append(q)
-                return q
-            else:
-                raise StopIteration
+            q = self.sequence[n - self.sequence[n - 1]] + self.sequence[n - self.sequence[n - 2]]
+            self.sequence.append(q)
+            return q
 
         except IndexError:
             raise StopIteration
 
 
-Q = Q_Hofstadter([1, 2])
+Q = QHofstadter([1, 1])
 print([next(Q) for _ in range(20)])
