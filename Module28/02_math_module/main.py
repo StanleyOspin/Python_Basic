221183ed5ea07f270dc1c74f0d565c3797b0a06c
Stanislav Osipkin <stan1976@mail.ru>
2022-03-14 19:19:58 +0500

    home_work_28_performed

--- Module28/02_math_module/main.py
+++ Module28/02_math_module/main.py
@@ -1,1 +1,29 @@
-# TODO здесь писать код
+import math
+
+
+class MyMath:
+
+    @classmethod
+    def circle_len(cls, radius: int) -> int or float:
+        return 2 * math.pi * radius
+
+    @classmethod
+    def circle_sq(cls, radius: int) -> int or float:
+        return math.pi * radius ** 2
+
+    @classmethod
+    def cube_volume(cls, edge: int) -> int or float:
+        return edge ** 3
+
+    @classmethod
+    def sphere_sq(cls, radius: int) -> int or float:
+        return math.pi * 4 * (radius ** 2)
+
+
+res_1 = MyMath.circle_len(radius=5)
+
+res_2 = MyMath.circle_sq(radius=6)
+
+print(res_1)
+
+print(res_2)
--- Module28/04_date/main.py
+++ Module28/04_date/main.py
@@ -1,1 +1,32 @@
-# TODO здесь писать код
+class Date:
+    """Класс Date - проверяет числа даты на корректность\
+конвертирует строку даты в объект класса Date, состоящий\
+из соответствующих числовых значений дня, месяца и года"""
+
+    def __init__(self, *args) -> None:
+        self.day, self.month, self.year = args
+
+    def __str__(self) -> str:
+        return 'День: {} \tМесяц: {} \tГод: {}'.format(self.day, self.month, self.year)
+
+    @classmethod
+    def parse_date_string(cls, date_str: str) -> map:
+        return (map(int, date_str.split('-')))
+
+    @classmethod
+    def from_string(cls, date_str: str) -> object:
+        assert cls.is_date_valid(date_str), 'date is wrong'
+        return cls(*cls.parse_date_string(date_str))
+
+    @classmethod
+    def is_date_valid(cls, date_str: str) -> bool:
+        day, month, year = cls.parse_date_string(date_str)
+        return 0 < day <= 31 and 0 < month <= 12 and year > 0
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print(Date.is_date_valid('10-12-2077'))
+
+print(Date.is_date_valid('40-12-2077'))
+
