60630b9db06b1b265c30e379d04242b4010c161e
Stanislav Osipkin <stan1976@mail.ru>
2022-04-21 19:43:17 +0500

    home_work_28_added_task_01

--- Module28/01_work_with_file_2/main.py
+++ Module28/01_work_with_file_2/main.py
@@ -1,1 +1,18 @@
-# TODO здесь писать код
+class File:
+    'Kласс File — контекстный менеджер для работы с файлами.'
+
+    def __init__(self, filename: str) -> None:
+        self.filename = filename
+        self.mode = 'w'
+
+    def __enter__(self) -> 'File':
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_value, exc_traceback) -> bool:
+        self.file.close()
+        return True
+
+
+with File('test.txt') as file:
+    file.write('Всем привет!')
