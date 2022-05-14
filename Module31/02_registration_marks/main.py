491211bcfc1f166b59f40c398e126d11b86c94af
Stanislav Osipkin <stan1976@mail.ru>
2022-05-14 09:53:31 +0500

    home_work_31_tasks_01_02_03

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -1,1 +1,12 @@
-# TODO здесь писать код
+import re
+from typing import List
+
+if __name__ == '__main__':
+    data = input('Пример перечня: ').split()
+    privat = r'\w\d{3}\w{2}\d{2,3}'
+    taxi = r'\w{2}\d{3}\d{2,3}'
+    privat_numbers: List[str] = list(filter(lambda x: re.fullmatch(privat, x), data))
+    taxi_numbers: List[str] = list(filter(lambda x: re.fullmatch(taxi, x), data))
+
+    print('Список номеров частных автомобилей:', privat_numbers)
+    print('Список номеров такси:', taxi_numbers)
--- Module31/03_breaking_bad/main.py
+++ Module31/03_breaking_bad/main.py
@@ -1,1 +1,41 @@
-# TODO здесь писать код
+import requests
+import json
+from typing import Dict, List
+
+if __name__ == '__main__':
+    deaths_request = requests.get('https://www.breakingbadapi.com/api/deaths')
+    data = json.loads(deaths_request.text)
+    with open('breaking_bad_file.json', 'w') as file:
+        json.dump(data, file, indent=4)
+
+    with open('breaking_bad_file.json', 'r') as file:
+        data_file_episod = json.load(file)
+        episode: Dict[str, int] = max(data_file_episod, key=lambda x: x.get('number_of_deaths'))
+
+    episode_request = requests.get('https://www.breakingbadapi.com/api/episodes')
+    id_episod_finding = json.loads(episode_request.text)
+
+    with open('episodes_file.json', 'w') as file:
+        json.dump(id_episod_finding, file, indent=4)
+
+    with open('episodes_file.json', 'r') as file:
+        data_file_id_episode = json.load(file)
+        id_of_episode: List[dict] = list(filter(
+            lambda x: int(x.get('episode')) == episode.get('episode') and int(x.get('season')) == episode.get('season'),
+            data_file_id_episode))
+        id_of_episode[0].update(episode)
+        result = {}
+
+        for item in id_of_episode:
+            result['episode_id'] = item.get('episode_id')
+            result['season'] = item.get('season')
+            result['episode'] = item.get('episode')
+            result['number_of_deaths'] = item.get('number_of_deaths')
+            result['death'] = item.get('death')
+
+    with open('result_file.json', 'w') as file:
+        json.dump(result, file, indent=4)
+
+    with open('result_file.json', 'r') as file:
+        data = json.load(file)
+        print(data)
