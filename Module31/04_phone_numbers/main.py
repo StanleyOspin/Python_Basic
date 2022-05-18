8703ca07f95b8eec7a83984c1d5f9b9a55fa8dd6
Stanislav Osipkin <stan1976@mail.ru>
2022-05-18 18:10:37 +0500

    home_work_31

--- Module31/04_phone_numbers/main.py
+++ Module31/04_phone_numbers/main.py
@@ -1,1 +1,13 @@
-# TODO здесь писать код
+import re
+import itertools
+from typing import List
+
+if __name__ == '__main__':
+    user_input = input('Введите список номеров через запятую: ').split(',')
+    pattern = r'\b[8-9]\d{9}'
+    numbers_check: List[str] = list(
+        map(lambda x: 'всё в порядке' if re.fullmatch(pattern, x) else 'не подходит', user_input))
+    order: List[str] = ['первый номер', 'второй номер', 'третий номер']
+    result = zip(itertools.cycle(order), numbers_check)
+    for item in result:
+        print('{}: {}'.format(item[0], item[1]))
--- Module31/06_web_scrapping/main.py
+++ Module31/06_web_scrapping/main.py
@@ -1,1 +1,9 @@
-# TODO здесь писать код
+import requests
+import re
+from typing import List
+
+if __name__ == '__main__':
+    html_text = requests.get('http://www.columbia.edu/~fdc/sample.html').text
+    pattern = r'<h3.*>(.+)</h3>'
+    result: List[str] = re.findall(pattern, html_text)
+    print(result)
